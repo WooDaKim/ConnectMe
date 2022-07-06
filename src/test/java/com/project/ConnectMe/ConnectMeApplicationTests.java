d94e39a46802cf53625608275139ec751e9f7393
wooda <woosskimgit@gmail.com>
2022-07-06 20:59:49 +0900

    프로젝트 등록

---
+++ src/test/java/com/project/ConnectMe/ConnectMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ConnectMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
